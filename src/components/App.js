ef006fd33f760eb4c798a6731382d7d182e0f1f8
vovaryba <vovaryba@gmail.com>
2022-04-05 17:15:28 +0300

    Сделал

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,69 @@
-import Profile from 'components/Profile/Profile';
-import user from 'components/Profile/user.json';
-import Container from 'components/Container/Container';
-import StatisticsList from 'components/StatisticsList/StatisticsList';
-import data from 'components/StatisticsList/data.json';
-import FriendList from 'components/FriendList/FriendList';
-import friends from 'components/FriendList/friends.json';
-import TransactionHistory from 'components/TransactionHistory/TransactionHistory';
-import transactions from 'components/TransactionHistory/transactions.json';
+import { Component } from 'react';
+import shortid from 'shortid';
+import Form from 'components/Form';
+import Filter from 'components/Filter';
+import List from 'components/List';
 
-function App() {
-  return (
-    <>
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-      <Container>
-        <StatisticsList title="Upload stats" stats={data} />
-        <StatisticsList stats={data} />
-      </Container>
-      <FriendList friends={friends} />
-      <TransactionHistory transactions={transactions} />
-    </>
-  );
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  addContact = data => {
+    const contact = {
+      id: shortid.generate(),
+      name: data.name,
+      number: data.number,
+    };
+
+    const { contacts } = this.state;
+    const notUniqueContact = contacts.find(ppl => ppl.name === contact.name);
+
+    if (notUniqueContact) {
+      alert(notUniqueContact.name + ' is already in contacts.');
+      return;
+    } else {
+      this.setState(({ contacts }) => ({
+        contacts: [contact, ...contacts],
+      }));
+    }
+  };
+
+  deleteContact = contactId => {
+    this.setState(({ contacts }) => ({
+      contacts: contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+  changeFilter = event => {
+    const { value } = event.currentTarget;
+    this.setState({ filter: value });
+  };
+
+  render() {
+    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    const visibleContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter),
+    );
+
+    return (
+      <>
+        <h1>Phonebook</h1>
+        <Form onSubmit={this.addContact} />
+
+        <h2>Contacts</h2>
+        <Filter value={filter} onChange={this.changeFilter} />
+        <List contacts={visibleContacts} onDeleteContact={this.deleteContact} />
+      </>
+    );
+  }
 }
 
 export default App;
